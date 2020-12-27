b0c2a7288f85544d3c6485270232275eb674c210
nitesh mishra <niteshmishra001@gmail.com>
2020-12-27 12:21:18 +0000

    AddProduct Model , push to db, update migration

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -17,5 +17,8 @@ namespace BulkyBook.DataAccess.Data
         public DbSet<Category> Categories { get; set; }
 
         public DbSet<CoverType> CoverTypes { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
     }
 }

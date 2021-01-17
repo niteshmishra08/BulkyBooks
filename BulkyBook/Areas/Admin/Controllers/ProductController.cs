a2c434579ce3342e9fa3388a3e1e6a1d916c63ea
nitesh mishra <niteshmishra001@gmail.com>
2021-01-17 12:54:39 +0000

    Product js and Upsert. Tinycloud for text editor

--- BulkyBook/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBook/Areas/Admin/Controllers/ProductController.cs
@@ -41,7 +41,7 @@ namespace BulkyBook.Areas.Admin.Controllers
                     Value = i.Id.ToString()
                 }),
 
-                CoverTypeList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
                 {
                     Text = i.Name,
                     Value = i.Id.ToString()
